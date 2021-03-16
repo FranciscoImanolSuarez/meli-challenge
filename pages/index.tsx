d690aa36018ac98a350b00e54a21103317f47901
FranciscoImanolSuarez <imanolsuarez98@gmail.com>
2021-03-16 11:32:00 -0300

    add link

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,46 +9,48 @@ interface Props {
   results: Product[];
 }
 
-const IndexPage: React.FC<Props> = ({results}) => {
-  console.log({results})
-  return <Box centerContent padding={4}>  
-<Stack width="100%" padding={4} backgroundColor="white" borderRadius={2} boxShadow="sm">
-    <Stack>
-      {results.map(product =>
-      <Link  key={product.id} href={`/${product.id}`}>
-      <Stack direction="row" justifyContent="space-between">
-      <Stack direction="row">
-      <Box 
-            backgroundColor="gray.50"
-            backgroundImage={`url(${product.image})`}
-            backgroundRepeat="no-repeat"
-            backgroundSize="contain"
-            backgroundPosition="center"
-            borderRadius="sm"
-            width={180} 
-            height={180} 
-            minWidth={180}
-            minHeight={180}
-            src={product.image}
-          />
-          <Stack>
-            <Text fontSize="2xl" fontWeight={500}>
-          {product.price.toLocaleString('es-AR', {style:'currency', currency:'ARS'})}
+const IndexPage: React.FC<Props> = ({ results }) => {
+  console.log({ results })
+  return <Box centerContent padding={4}>
+    <Stack width="100%" padding={4} backgroundColor="white" borderRadius={2} boxShadow="sm">
+      <Stack>
+        {results.map(product =>
+          <Link key={product.id} href={`/${product.id}`}>
+            <a>
+              <Stack direction="row" justifyContent="space-between">
+                <Stack direction="row">
+                  <Box
+                    backgroundColor="gray.50"
+                    backgroundImage={`url(${product.image})`}
+                    backgroundRepeat="no-repeat"
+                    backgroundSize="contain"
+                    backgroundPosition="center"
+                    borderRadius="sm"
+                    width={180}
+                    height={180}
+                    minWidth={180}
+                    minHeight={180}
+                    src={product.image}
+                  />
+                  <Stack>
+                    <Text fontSize="2xl" fontWeight={500}>
+                      {product.price.toLocaleString('es-AR', { style: 'currency', currency: 'ARS' })}
 
-            </Text>
-            <Text>{product.title}</Text>
-          </Stack>
-        </Stack>  
-         <Text fontSize="sm">{product.location}</Text>
+                    </Text>
+                    <Text>{product.title}</Text>
+                  </Stack>
+                </Stack>
+                <Text fontSize="sm">{product.location}</Text>
+              </Stack>
+            </a>
+          </Link>
+        )}
       </Stack>
-      </Link>
-      )}
     </Stack>
-  </Stack>
   </Box>
 }
 
-export const getServerSideProps: GetServerSideProps = async ({query} ) => {
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   const results = await api.search(query.q as string);
 
   return {
