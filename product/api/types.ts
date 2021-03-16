2247dbc56dfe30a43c01584c196df3fd405ba207
FranciscoImanolSuarez <imanolsuarez98@gmail.com>
2021-03-16 12:14:46 -0300

    add page product

--- product/api/types.ts
+++ product/api/types.ts
@@ -1,13 +1,25 @@
-export interface RawProduct {
+export interface RawProductSearch {
     id: string;
     title: string;
     price: number;
     thumbnail: string;
     address: {
-        state_name: string;
-    }
-}
-
-export interface MercadoLibreResponse {
-    results: RawProduct
-}
+      state_name: string;
+    };
+  }
+  
+  export interface RawProduct {
+    id: string;
+    title: string;
+    price: number;
+    thumbnail: string;
+    seller_address: {
+      state: {
+        name: string;
+      };
+    };
+  }
+  
+  export interface MercadoLibreSearchResponse {
+    results: RawProductSearch[];
+  }
